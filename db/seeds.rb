1c58c35919eb248af84b4e92ebbe55d45864688b
Patrick Cook <pcookcollege@gmail.com>
2019-09-27 10:43:15 -0700

    Updated seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,18 @@ admin = User.create(username: "admin", password_hash: "admin_password_hash")
 
 admin_queue = admin.group_queues.create(name: "admin_queue")
 
+admin_queue2 = admin.group_queues.create(name: "admin_2nd_queue")
+
 admin_queue.songs.create(
   title: "Never Let Me Go",
   artist: "Attom",
   album_image_url: "https://i.scdn.co/image/a1fa07fbc51393d2f60ae631af9ec0f3797c84d7",
-  spotify_uri: ""
+  spotify_uri: "spotify:track:0Pz5g9IWTlZbVCdxyER2Su"
+)
+
+admin_queue2.songs.create(
+  title: "Staring at the Sun",
+  artist: "Post Malone",
+  album_image_url: "https://i.scdn.co/image/a1fa07fbc51393d2f60ae631af9ec0f3797c84d7",
+  spotify_uri: "spotify:track:2leIuE4fdcJoNMSKu5Yr7a"
 )
