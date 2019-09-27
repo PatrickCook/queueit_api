be56d72e42fbdbe37a04eeb0e5d4f14062e9f52b
Patrick Cook <pcookcollege@gmail.com>
2019-09-26 17:03:50 -0700

    Added user, queue, and song models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'queue/index'
+  get 'queue/new'
+  get 'queue/create'
+  get 'queue/show'
+  get 'queue/update'
+  get 'queue/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  
 end
