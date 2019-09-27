96f37147983fee3e9ec94e94eb0971ff09ca008c
Patrick Cook <pcookcollege@gmail.com>
2019-09-27 10:59:24 -0700

    Created basic routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'queue/index'
-  get 'queue/new'
-  get 'queue/create'
-  get 'queue/show'
-  get 'queue/update'
-  get 'queue/destroy'
-
+  namespace :api do
+    resources :users, :group_queues, :songs, :votes
+  end
 end
