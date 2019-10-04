2e13f42bae138c3d00b4c210eacdfed85500e6b5
Patrick Cook <pcookcollege@gmail.com>
2019-10-04 13:54:38 -0700

    move modifications to new branch

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -15,32 +15,17 @@ module Api
     end
 
     def create
-      @user = User.new(user_params)
-      if @user.save
-        render :json => {
-          :status => "ok",
-          :user => @user.to_json(:only => [:id, :username])
-        }
+      user = User.new(user_params)
+      if user.save
+        render json: { status: "ok", user: user.to_json(:only => [:id, :username]) }
       else
-        render :json => {
-          :status => "ok",
-          :user => @user.to_json(:only => [:id, :username])
-        }
+        render json: { status: "error", message: user.errors.full_messages }
       end
-
-      render :json => {
-        :status => "ok",
-        :user => @user.to_json(:only => [:id, :username])
-      }
     end
 
     def show
       user = User.find(params[:id])
-
-      render :json => {
-        :status => "ok",
-        :user => user.to_json(:only => [:id, :username])
-      }
+      render json: { status: "ok", user: user.to_json(:only => [:id, :username]) }
     end
 
     def edit
