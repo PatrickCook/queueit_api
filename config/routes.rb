ae497e671e39828f779d246de304cbab2412f1c5
Patrick Cook <pcookcollege@gmail.com>
2019-10-12 19:14:30 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "api#index"
+
   post 'authenticate', to: 'authentication#authenticate'
+
+  resources :users, only: [:index, :create, :update, :destroy]
+
+  resources :group_queues, only: [:index, :create, :update, :destroy] do
+    post "/join", to: "groups#join"
+    post "/leave", to: "groups#leave"
+    
+    resources :songs, only: [:index, :create, :destroy] do
+      post "/upvote", to: "votes#create"
+      delete "/downvote", to: "votes#destroy"
+    end
+  end
 end
